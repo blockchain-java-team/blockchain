70578132545fe87989cdd0c3f8a550c0ad31e80d
AbdessadekHm <abdessadekhm@gmail.com>
2024-12-09 23:03:05 +0100

    Add PeerRequest Basic Functionality

---
+++ src/main/java/com/blockchain/threads/PeerRequestThread.java
@@ -0,0 +1,14 @@
+package com.blockchain.threads;
+
+import java.net.Socket;
+
+public class PeerRequestThread {
+
+    private Socket socket;
+
+    public PeerRequestThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    
+}
