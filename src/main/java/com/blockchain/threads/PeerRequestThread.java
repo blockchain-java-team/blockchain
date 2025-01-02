324b8ab1d4d5bf77d3b3c71c428bbd08c15c6e1a
AbdessadekHm <abdessadekhm@gmail.com>
2025-01-02 09:56:25 +0100

    Add Net Functionnalities to dev

---
+++ src/main/java/com/blockchain/threads/PeerRequestThread.java
@@ -0,0 +1,37 @@
+package com.blockchain.threads;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.LinkedList;
+
+import com.blockchain.model.Block;
+import com.blockchain.service.BlockchainData;
+
+public class PeerRequestThread extends Thread {
+
+    private Socket socket;
+
+    public PeerRequestThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            ObjectOutputStream objectOutput = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream objectInput = new ObjectInputStream(socket.getInputStream());
+
+            LinkedList<Block> recievedBC = (LinkedList<Block>) objectInput.readObject();
+            System.out.println("LedgerId = " + recievedBC.getLast().getLedgerId()  +
+                    " Size= " + recievedBC.getLast().getTransactionLedger().size());
+           objectOutput.writeObject(BlockchainData.getInstance().getBlockchainConsensus(recievedBC));
+        } catch (IOException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    
+}
